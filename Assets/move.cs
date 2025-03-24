39bdc19884b314872a36e913d4fafcbc674717d9
ali18shams <kiren.master18@gmail.com>
2025-03-25 02:58:16 +0330

    2
    harekat bocse zer

---
+++ Assets/move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class move : MonoBehaviour
+{
+	private Rigidbody2D rigid;
+	public float movv;
+	void Start()
+	
+    {
+	    rigid =GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if(Input.GetKey(KeyCode.D))
+	    {rigid.linearVelocity=new Vector2(movv,0);}
+    }
+}
